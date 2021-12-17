37971ba956484c71e5e9ebf8d1596f7d0eced122
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-12-17 14:15:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    width: 350,
+    height: 300,
+    image_format: 'jpeg',
+    jpeg_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function capture() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("output").innerHTML = "<img id='captured_image' src="+data_uri+">";
+    });
+}
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/KwN8fVIuj/model.json',modelLoaded);
+
+function modelLoaded() {
+    console.log("model Loaded");
+}
