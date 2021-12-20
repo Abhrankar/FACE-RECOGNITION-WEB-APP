b48a72b3c03f604bc9e33098affe2472e9342421
Abhrankar <84310569+Abhrankar@users.noreply.github.com>
2021-12-20 16:49:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,3 +20,19 @@ classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models
 function modelLoaded() {
     console.log("model Loaded");
 }
+
+function identify() {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, result) {
+    if (error) {
+        //console.error(error);
+    } else {
+        console.log(result);
+        document.getElementById("result_object_name").innerHTML = result[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = result[0].confidence.toFixed(2);
+    }
+
+}
